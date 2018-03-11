c45a9ab9f6e6fc34b9013d9994de3d9570352f2c
Matt Downs <matthewrdowns@gmail.com>
2018-03-11 11:54:20 -0400

    uploading web

---
+++ web/application.py
@@ -0,0 +1,13 @@
+from random import randint
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', rand=randint(1,1000))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(threaded=True)
