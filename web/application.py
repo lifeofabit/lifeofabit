9655174cfc7d9e85598754b4b52277604520fe83
Matt Downs <matthewrdowns@gmail.com>
2018-03-11 12:34:00 -0400

    under construction

--- web/application.py
+++ web/application.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('construction.html')
+    # return render_template('base.html', rand=randint(1,1000))
+
+@app.route('/test')
+def test():
     return render_template('base.html', rand=randint(1,1000))
 
 if __name__ == '__main__':
